b7f3ad6d95e2657e5814e9db6b7c44469675dd85
WildCatNya <73734059+WildCatNya@users.noreply.github.com>
2023-11-22 11:46:54 +0700

    Delete Feedback.Api directory

--- Feedback.Api/Database/Entities/Common/Entity.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Feedback.Api.Database.Entities.Common;
-
-public abstract class Entity
-{
-    public int Id { get; set; }
-}
--- Feedback.Api/Database/Entities/Subject.cs
+++
@@ -1,5 +0,0 @@
-﻿using Feedback.Api.Database.Entities.Common;
-
-namespace Feedback.Api.Database.Entities;
-
-public class Subject : DictionaryEntity { }
