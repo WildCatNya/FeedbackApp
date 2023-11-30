52013b46ecedfcc52389b8786ee4087186826448
Kirill <Kirill@DESKTOP-V2TRQ0M>
2023-11-30 20:47:27 +0700

    Добавил файл с именами ролей, немного преобразовал действия с ними

---
+++ Feedback.Server/Helpers/RolesNames.cs
@@ -0,0 +1,39 @@
+﻿namespace Feedback.Server.Helpers;
+
+public static class RolesNames
+{
+    /// <summary>
+    /// Создатель, тот кто может делать все
+    /// </summary>
+    public const string Creator = "Creator";
+
+    /// <summary>
+    /// Может делать многое, но не все
+    /// </summary>
+    public const string Admin = "Admin";
+
+    /// <summary>
+    /// Может видеть все, но не спрятанное
+    /// </summary>
+    public const string AllSeeing = "AllSeeing";
+
+    /// <summary>
+    /// Может отменять выполнение заявок
+    /// </summary>
+    public const string CanCancel = "CanCancel";
+
+    /// <summary>
+    /// Может прятать заявки
+    /// </summary>
+    public const string CanConceal = "CanConceal";
+
+    /// <summary>
+    /// Может удалять заявки
+    /// </summary>
+    public const string CanDelete = "CanDelete";
+
+    /// <summary>
+    /// Может производить любые манипуляции с пользователем
+    /// </summary>
+    public const string UserFullAccess = "UserFullAccess";
+}
