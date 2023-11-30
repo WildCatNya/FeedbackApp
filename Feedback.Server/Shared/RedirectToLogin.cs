cfaa35b96e6d2336ff6f268c90c56742a9ffa21f
Кирилл Кудей <kudey_kir@mail.ru>
2023-11-30 10:22:40 +0700

    Добавил колбек для ререндера значений MudBadge, добавил редирект, если пользователь не авторизирован

---
+++ Feedback.Server/Shared/RedirectToLogin.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Feedback.Server.Shared;
+
+public sealed class RedirectToLogin : ComponentBase
+{
+    [Inject] private NavigationManager _navigationManager { get; set; }        
+
+    protected override void OnAfterRender(bool firstRender) =>
+        _navigationManager.NavigateTo("/account/login", true);
+}
